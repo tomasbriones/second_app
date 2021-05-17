2f3b3c700c35b7bbede1ec8b822db6de1d732279
tomas briones <tomasbriones@gmail.com>
2021-05-17 09:00:22 -0400

    page/index view and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
-  root "pages#index"
+  get  "pages/index"
+  #root "pages#index"
 end
