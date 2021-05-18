4f5bad556af2c1d781d35de44264b95be538029a
tomas briones <tomasbriones@gmail.com>
2021-05-17 22:28:16 -0400

    add test view and linkto helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/login'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
   get  "pages/index"
